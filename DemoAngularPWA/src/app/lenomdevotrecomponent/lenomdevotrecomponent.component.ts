c48107b9f7638463cccffac7219fc48e5b38de9f
Nathan <nathan.pire@gmail.com>
2019-12-11 09:38:24 +0100

    Adding pictures and editing app

--- DemoAngularPWA/src/app/lenomdevotrecomponent/lenomdevotrecomponent.component.ts
+++ DemoAngularPWA/src/app/lenomdevotrecomponent/lenomdevotrecomponent.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ThisisaserviceService } from '../thisisaservice.service';
 
 @Component({
   selector: 'app-lenomdevotrecomponent',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LenomdevotrecomponentComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private thisIsAService: ThisisaserviceService
+  ) { }
 
   ngOnInit() {
   }
