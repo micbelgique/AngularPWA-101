85da3a498a15624ec2dcc0839fd3911c280212de
Nathan <nathan.pire@gmail.com>
2019-12-11 09:16:28 +0100

    editing app and adding pictures

--- DemoAngularPWA/src/app/thisisaservice.service.ts
+++ DemoAngularPWA/src/app/thisisaservice.service.ts
@@ -5,10 +5,18 @@ import { Injectable } from '@angular/core';
 })
 export class ThisisaserviceService {
 
-  private arr: [];
+  private arr: string[];
   constructor() { }
 
   public getData(): any {
     return this.arr;
+  }
+  public addData(data: string): boolean {
+    if (data.length > 0) {
+      this.arr.push(data);
+      return true;
+    } else {
+      return false;
+    }
   }
 }
